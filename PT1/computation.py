84d044bc1cea474e08f57eb64ef146a6ece76979
Uzziel Kyle <101367055+UzziKyle@users.noreply.github.com>
2023-02-09 11:26:47 +0800

    Add files via upload

---
+++ PT1/computation.py
@@ -0,0 +1,16 @@
+def tuitionComp(studInfo):
+
+    if studInfo[0] == "BSIT":
+        firstPayment = int(input("First Payment: "))
+        balance = 12500 - firstPayment
+
+        print("\n",studInfo[1],",",studInfo[3],",",studInfo[2],",",studInfo[4],". Balance: ",balance)
+
+    elif studInfo[0] == "BSCS":
+        firstPayment = int(input("First Payment: "))
+        balance = 13500 - firstPayment
+
+        print("\n",studInfo[1],",",studInfo[3],",",studInfo[2],",",studInfo[4],". Balance: ",balance)
+   
+    else:
+        print("Invalid")
---
+++ PT1/main.py
@@ -0,0 +1,11 @@
+import computation
+
+studInfo = []
+
+studInfo.append(input("Enter the Course: "))
+studInfo.append(input("Enter the IDNo: "))
+studInfo.append(input("Firstname: "))
+studInfo.append(input("Lastname: "))
+studInfo.append(input("Middle Initial: "))
+
+computation.tuitionComp(studInfo)
